b666a3d6e82eebc51291e5c806b3176df5cee932
vankatwijk <hendrikus.hpvk@gmail.com>
2020-04-18 15:30:53 -0800

    init project

---
+++ app/main.js
@@ -0,0 +1,21 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+import VueDevtools from 'nativescript-vue-devtools'
+
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
+import store from './store'
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
+
+new Vue({
+  store,
+  render: h => h('frame', [h(App)])
+}).$start()
